63147913e128ab0b1e70d72aaf940d759e04c092
Nando Davis <nandodavis@gmail.com>
2022-10-05 11:12:37 +0200

    Add url parameter to edit_in_language_button

--- src/wagtailtrans/wagtail_hooks.py
+++ src/wagtailtrans/wagtail_hooks.py
@@ -136,7 +136,7 @@ def hide_non_canonical_languages(parent_page, pages, request):
 
 
 @hooks.register('register_page_listing_buttons')
-def edit_in_language_button(page, page_perms, is_parent=False):
+def edit_in_language_button(page, page_perms, is_parent=False, url=None):
     """Add ``Edit in`` button to the page explorer.
 
     When hiding all other translation except the canonical language, which is
